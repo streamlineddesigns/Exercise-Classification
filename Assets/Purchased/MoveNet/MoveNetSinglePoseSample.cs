4184d2a6694c134549d6aa6302c2cfbd4fcc16fd
streamlineddesigns <streamline.our.designs@gmail.com>
2023-11-27 23:27:03 -0500

    initial commit

---
+++ Assets/Purchased/MoveNet/MoveNetSinglePoseSample.cs
@@ -0,0 +1,91 @@
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using TensorFlowLite;
+using TensorFlowLite.MoveNet;
+
+[RequireComponent(typeof(WebCamInput))]
+public class MoveNetSinglePoseSample : MonoBehaviour
+{
+    public List<Vector3> poses = new List<Vector3>(17);
+
+    [SerializeField]
+    MoveNetSinglePose.Options options = default;
+
+    [SerializeField]
+    private RectTransform cameraView = null;
+
+    [SerializeField]
+    private bool runBackground = false;
+
+    [SerializeField, Range(0, 1)]
+    private float threshold = 0.3f;
+
+    private MoveNetSinglePose moveNet;
+    private MoveNetPose pose;
+    private MoveNetDrawer drawer;
+
+    private UniTask<bool> task;
+    private CancellationToken cancellationToken;
+
+    private void Start()
+    {
+        moveNet = new MoveNetSinglePose(options);
+        drawer = new MoveNetDrawer(Camera.main, cameraView);
+
+        cancellationToken = this.GetCancellationTokenOnDestroy();
+
+        var webCamInput = GetComponent<WebCamInput>();
+        webCamInput.OnTextureUpdate.AddListener(OnTextureUpdate);
+    }
+
+    private void OnDestroy()
+    {
+        var webCamInput = GetComponent<WebCamInput>();
+        webCamInput.OnTextureUpdate.RemoveListener(OnTextureUpdate);
+        moveNet?.Dispose();
+        drawer?.Dispose();
+    }
+
+    private void Update()
+    {
+        if (pose != null)
+        {
+            drawer.DrawPose(pose, threshold);
+            
+            for (int i = 0; i < pose.Length; i++)
+            {
+                poses[i] = new Vector3(pose[i].x, pose[i].y, pose[i].score);
+            }
+        }
+    }
+
+    private void OnTextureUpdate(Texture texture)
+    {
+        if (runBackground)
+        {
+            if (task.Status.IsCompleted())
+            {
+                task = InvokeAsync(texture);
+            }
+        }
+        else
+        {
+            Invoke(texture);
+        }
+    }
+
+    private void Invoke(Texture texture)
+    {
+        moveNet.Invoke(texture);
+        pose = moveNet.GetResult();
+    }
+
+    private async UniTask<bool> InvokeAsync(Texture texture)
+    {
+        await moveNet.InvokeAsync(texture, cancellationToken);
+        pose = moveNet.GetResult();
+        return true;
+    }
+}
