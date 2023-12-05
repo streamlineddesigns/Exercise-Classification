6e011fba846dfe2c847b68b3ddc614d1655843eb
streamlineddesigns <streamline.our.designs@gmail.com>
2023-12-05 00:27:37 -0500

    Created training scripts

---
+++ Trainer.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+from sklearn.metrics import classification_report
+from tensorflow import keras
+from keras import layers
+import tensorflow as tf
+
+# Read in the CSV file
+df = pd.read_csv('data.csv')
+
+# Sliding window function
+def sliding_windows(data, seq_length, target_pos):
+    X,Y = [], []
+    for i in range(len(data)-seq_length):
+        X.append(data.iloc[i : i+seq_length, :target_pos])
+        Y.append(data.iloc[i+seq_length, target_pos:])
+    return np.array(X), np.array(Y)
+
+# Prepare training data
+seq_length = 8
+target_pos = -3 # Third last columns as target
+X, Y = sliding_windows(df, seq_length, target_pos)
+
+#Split into training / test
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+
+# Build LSTM
+model = Sequential()
+model.add(LSTM(32, kernel_regularizer=keras.regularizers.l2(0.001), input_shape=(seq_length, X.shape[-1]), return_sequences=True))
+model.add(layers.Dropout(0.2))
+model.add(LSTM(16, kernel_regularizer=keras.regularizers.l2(0.001)))
+model.add(layers.Dropout(0.2))
+model.add(Dense(Y.shape[-1], activation='softmax'))
+
+# Compile and train
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+model.fit(X_train, y_train, epochs=128, batch_size=32)
+
+#Model info
+print("_______________________________________________________________________")
+print("Model Info")
+print("_______________________________________________________________________")
+print("X shape:", X.shape)
+print("Y shape:", Y.shape)
+model.summary()
+
+#Classification report info - Train
+print("_______________________________________________________________________")
+print("Train Info")
+print("_______________________________________________________________________")
+predictions_train = model.predict(X_train)
+pred_classes_train = np.argmax(predictions_train, axis=1)
+true_classes_train = np.argmax(y_train, axis=1)
+print(classification_report(true_classes_train, pred_classes_train))
+
+#Classification report info - Test
+print("_______________________________________________________________________")
+print("Test Info")
+print("_______________________________________________________________________")
+predictions_test = model.predict(X_test)
+pred_classes_test = np.argmax(predictions_test, axis=1)
+true_classes_test = np.argmax(y_test, axis=1)
+print(classification_report(true_classes_test, pred_classes_test))
+
+# Export the model
+tf.saved_model.save(model, "saved_model_dir")
---
+++ server.sh
@@ -0,0 +1,19 @@
+sudo apt update
+
+wget "https://repo.anaconda.com/miniconda/Miniconda3-py37_4.12.0-Linux-x86_64.sh"
+chmod 777 Miniconda3-py37_4.12.0-Linux-x86_64.sh
+./Miniconda3-py37_4.12.0-Linux-x86_64.sh
+
+conda create -n ml python=3.7
+conda activate ml
+
+pip install numpy
+pip install pandas
+pip install keras
+pip install tensorflow
+pip install scikit-learn
+pip install tf2onnx
+
+python Trainer.py
+python -m tensorflow.python.tools.saved_model_cli show --dir "./saved_model_dir" --tag serve --signature_def serving_default
+python -m tf2onnx.convert --saved-model "./saved_model_dir"  --opset 9 --inputs lstm_input:0 --inputs-as-nchw lstm_input:0 --output model.onnx
